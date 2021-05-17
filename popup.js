b44575e8599b4034add7905be4b132c79eb21d1d
MDerv <69446661+MDerv@users.noreply.github.com>
2021-05-16 18:14:24 -0600

    Adding all the project's files

---
+++ popup.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+function changePopup(){
+	alert("Hi! This is a popup!");
+	window.location.href="neterrorMario.html";
+    //chrome.browserAction.setPopup({popup: "/neterrorMario.html"});
+}
